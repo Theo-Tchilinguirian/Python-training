a8b841e3801dddf8999df983e1073fa662a2cc0f
Théo Tchilinguirian <60852187+theotchlx@users.noreply.github.com>
2023-01-24 00:32:55 +0100

    Update pygame_game_cat.py

--- Python training/Some games I tried making/pygame_game_cat.py
+++ Python training/Some games I tried making/pygame_game_cat.py
@@ -3,7 +3,7 @@ from random import randint
 pygame.init()
 
 size = width, height = 1000, 1000
-speed = [10, 1]
+speed = [2, 1]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -11,21 +11,28 @@ screen = pygame.display.set_mode(size)
 ball = pygame.image.load("pygame_game_funny_cat_photo.png")
 ballrect = ball.get_rect()
 
-while 1:
+
+running = True
+
+while running:
     r = randint(0, 100)
-    if r == 0:
-        clr = (255, 255, 255)
-    else:
-        clr = black
+    bckgrnd_colour = black
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
+    ballrect = ballrect.move(speed)  # conditions pour inverser la vitesse si on atteint le bord :
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
 
-    screen.fill(clr)
+    screen.fill(bckgrnd_colour)
     screen.blit(ball, ballrect)
     pygame.display.flip()
+    
+    # Le joueur ferme la fenêtre :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+        print(event)
